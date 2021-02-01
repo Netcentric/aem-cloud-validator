6af8a18eadad28ceb8ee9c54a9678c5198f5e7d8
Konrad Windszus <konrad.windszus@netcentric.biz>
2021-02-01 11:03:14 +0100

    Add rule to prevent mutable content packages containing install hooks (#4)
    This closes #3

--- src/main/java/biz/netcentric/filevault/validator/aem/cloud/AemCloudValidator.java
+++ src/main/java/biz/netcentric/filevault/validator/aem/cloud/AemCloudValidator.java
@@ -1,6 +1,7 @@
 package biz.netcentric.filevault.validator.aem.cloud;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /*-
  * #%L
@@ -18,6 +19,9 @@ import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Collections;
 
+import org.apache.jackrabbit.vault.packaging.PackageType;
+import org.apache.jackrabbit.vault.util.Constants;
+import org.apache.jackrabbit.vault.validation.spi.MetaInfPathValidator;
 import org.apache.jackrabbit.vault.validation.spi.NodePathValidator;
 import org.apache.jackrabbit.vault.validation.spi.ValidationContext;
 import org.apache.jackrabbit.vault.validation.spi.ValidationMessage;
@@ -25,24 +29,33 @@ import org.apache.jackrabbit.vault.validation.spi.ValidationMessageSeverity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class AemCloudValidator implements NodePathValidator {
+public class AemCloudValidator implements NodePathValidator, MetaInfPathValidator {
 
     static final String VIOLATION_MESSAGE_STRING_VAR_NODES_CONDITION_CONTAINER = "only allowed in author-specific packages";
     static final String VIOLATION_MESSAGE_STRING_VAR_NODES_CONDITION_OVERALL = "not allowed";
     static final String VIOLATION_MESSAGE_STRING_VAR_NODES = "Using nodes below /var is %s. Consider to use repoinit scripts instead or move that content to another location. Further details at https://experienceleague.adobe.com/docs/experience-manager-learn/cloud-service/debugging/debugging-aem-as-a-cloud-service/build-and-deployment.html?lang=en#including-%%2Fvar-in-content-package";
+    static final String VIOLATION_MESSAGE_INSTALL_HOOK_IN_MUTABLE_PACKAGE = "Using install hooks in mutable content packages leads to deployment failures as the underlying service user on the publish does not have the right to execute those.";
+
+    // this path is relative to META-INF
+    private static final Path INSTALL_HOOK_PATH = Paths.get(Constants.VAULT_DIR, Constants.HOOKS_DIR);
 
     private final @NotNull ValidationMessageSeverity defaultSeverity;
     private final ValidationContext containerValidationContext;
+    private final PackageType packageType;
     private boolean foundViolation;
-
+    private boolean hasMutableNodes;
     private boolean allowVarNodesOutsideContainers;
+    private boolean hasInstallHooks;
 
-    public AemCloudValidator(boolean allowVarNodesOutsideContainers, @Nullable ValidationContext containerValidationContext, @NotNull ValidationMessageSeverity defaultSeverity) {
+    public AemCloudValidator(boolean allowVarNodesOutsideContainers, @Nullable PackageType packageType, @Nullable ValidationContext containerValidationContext, @NotNull ValidationMessageSeverity defaultSeverity) {
         super();
+        this.packageType = packageType;
         this.containerValidationContext = containerValidationContext;
         this.defaultSeverity = defaultSeverity;
         this.foundViolation = false;
         this.allowVarNodesOutsideContainers = allowVarNodesOutsideContainers;
+        this.hasMutableNodes = false;
+        this.hasInstallHooks = false;
     }
 
     @Override
@@ -55,6 +68,9 @@ public class AemCloudValidator implements NodePathValidator {
                 return Collections.singleton(new ValidationMessage(defaultSeverity, String.format(
                         VIOLATION_MESSAGE_STRING_VAR_NODES, allowVarNodesOutsideContainers ? VIOLATION_MESSAGE_STRING_VAR_NODES_CONDITION_CONTAINER : VIOLATION_MESSAGE_STRING_VAR_NODES_CONDITION_OVERALL)));
             }
+        }
+        if (!path.startsWith("/apps/") && !path.startsWith("/libs")) {
+            hasMutableNodes = true;
         }
         return null;
     }
@@ -76,7 +92,7 @@ public class AemCloudValidator implements NodePathValidator {
             }
         }
     }
-    
+
     static boolean isPackagePathInstalledConditionally(String runMode, Path packageRootPath) {
         // https://experienceleague.adobe.com/docs/experience-manager-cloud-service/implementing/developing/aem-project-content-package-structure.html?lang=en#embeddeds
         for (int i = packageRootPath.getNameCount() - 1; i-- > 0; ) {
@@ -89,6 +105,24 @@ public class AemCloudValidator implements NodePathValidator {
 
     @Override
     public @Nullable Collection<ValidationMessage> done() {
+        if (hasInstallHooks && hasMutableNodes) {
+            return Collections.singleton(new ValidationMessage(defaultSeverity, VIOLATION_MESSAGE_INSTALL_HOOK_IN_MUTABLE_PACKAGE));
+        }
+        return null;
+    }
+
+    @Override
+    public @Nullable Collection<ValidationMessage> validateMetaInfPath(@NotNull Path filePath) {
+        // is it install hook?
+        if (filePath.startsWith(INSTALL_HOOK_PATH) && filePath.toString().endsWith(".jar")) {
+            // is it mutable content package?
+            if (PackageType.CONTENT.equals(packageType) || PackageType.MIXED.equals(packageType)) {
+                return Collections.singleton(new ValidationMessage(defaultSeverity, VIOLATION_MESSAGE_INSTALL_HOOK_IN_MUTABLE_PACKAGE));
+            } else if (packageType == null) {
+                // defer checking until one is sure that the package has mutable content
+                hasInstallHooks = true;
+            }
+        }
         return null;
     }
 
