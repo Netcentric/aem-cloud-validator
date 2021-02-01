0de8e971d868a6ce99b78bb41c905c6079b286c4
Konrad Windszus <konrad.windszus@netcentric.biz>
2021-02-01 12:56:49 +0100

    Prevent Oak Index Definitions of type != lucene and enforce naming (#7)
    schema
    
    This closes #6
    This closes #5

--- src/main/java/biz/netcentric/filevault/validator/aem/cloud/AemCloudValidator.java
+++ src/main/java/biz/netcentric/filevault/validator/aem/cloud/AemCloudValidator.java
@@ -2,6 +2,7 @@ package biz.netcentric.filevault.validator.aem.cloud;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /*-
  * #%L
@@ -18,9 +19,12 @@ import java.nio.file.Paths;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.regex.Pattern;
 
 import org.apache.jackrabbit.vault.packaging.PackageType;
 import org.apache.jackrabbit.vault.util.Constants;
+import org.apache.jackrabbit.vault.util.DocViewNode;
+import org.apache.jackrabbit.vault.validation.spi.DocumentViewXmlValidator;
 import org.apache.jackrabbit.vault.validation.spi.MetaInfPathValidator;
 import org.apache.jackrabbit.vault.validation.spi.NodePathValidator;
 import org.apache.jackrabbit.vault.validation.spi.ValidationContext;
@@ -29,16 +33,20 @@ import org.apache.jackrabbit.vault.validation.spi.ValidationMessageSeverity;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class AemCloudValidator implements NodePathValidator, MetaInfPathValidator {
+public class AemCloudValidator implements NodePathValidator, MetaInfPathValidator, DocumentViewXmlValidator {
 
     static final String VIOLATION_MESSAGE_STRING_VAR_NODES_CONDITION_CONTAINER = "only allowed in author-specific packages";
     static final String VIOLATION_MESSAGE_STRING_VAR_NODES_CONDITION_OVERALL = "not allowed";
     static final String VIOLATION_MESSAGE_STRING_VAR_NODES = "Using nodes below /var is %s. Consider to use repoinit scripts instead or move that content to another location. Further details at https://experienceleague.adobe.com/docs/experience-manager-learn/cloud-service/debugging/debugging-aem-as-a-cloud-service/build-and-deployment.html?lang=en#including-%%2Fvar-in-content-package";
     static final String VIOLATION_MESSAGE_INSTALL_HOOK_IN_MUTABLE_PACKAGE = "Using install hooks in mutable content packages leads to deployment failures as the underlying service user on the publish does not have the right to execute those.";
+    static final String VIOLATION_MESSAGE_INVALID_INDEX_DEFINITION_NODE_NAME = "All Oak index definition node names must end with '-custom-<integer>' but found name '%s'. Further details at https://experienceleague.adobe.com/docs/experience-manager-cloud-service/operations/indexing.html?lang=en#how-to-use";
 
     // this path is relative to META-INF
     private static final Path INSTALL_HOOK_PATH = Paths.get(Constants.VAULT_DIR, Constants.HOOKS_DIR);
+    private static final @NotNull String VIOLATION_MESSAGE_NON_LUCENE_TYPE_INDEX_DEFINITION = "Only oak:QueryIndexDefinitions of type='lucene' are supported in AEMaaCS but found type='%s'. Compare with https://experienceleague.adobe.com/docs/experience-manager-cloud-service/operations/indexing.html?lang=en#changes-in-aem-as-a-cloud-service";
 
+    private static final Pattern INDEX_DEFINITION_NAME_PATTERN = Pattern.compile(".*-custom-\\d++");
+    
     private final @NotNull ValidationMessageSeverity defaultSeverity;
     private final ValidationContext containerValidationContext;
     private final PackageType packageType;
@@ -122,6 +130,24 @@ public class AemCloudValidator implements NodePathValidator, MetaInfPathValidato
                 // defer checking until one is sure that the package has mutable content
                 hasInstallHooks = true;
             }
+        }
+        return null;
+    }
+
+    @Override
+    public @Nullable Collection<ValidationMessage> validate(@NotNull DocViewNode node, @NotNull String nodePath,
+            @NotNull Path filePath, boolean isRoot) {
+        if ("oak:QueryIndexDefinition".equals(node.primary)) {
+            Collection<ValidationMessage> messages = new ArrayList<>();
+            String indexType = node.getValue("{}type");
+            if (!"lucene".equals(indexType)) {
+                messages.add(new ValidationMessage(defaultSeverity, String.format(VIOLATION_MESSAGE_NON_LUCENE_TYPE_INDEX_DEFINITION, indexType)));
+            }
+            // check node name
+            if (!INDEX_DEFINITION_NAME_PATTERN.matcher(node.name).matches()) {
+                messages.add(new ValidationMessage(defaultSeverity, String.format(VIOLATION_MESSAGE_INVALID_INDEX_DEFINITION_NODE_NAME, node.name)));
+            }
+            return messages;
         }
         return null;
     }
