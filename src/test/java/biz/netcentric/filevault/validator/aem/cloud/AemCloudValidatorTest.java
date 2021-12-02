e48216e47a9f3484afc0071d91643cbb643bce70
Konrad Windszus <konrad.windszus@netcentric.biz>
2021-12-02 19:44:26 +0100

    re-add license header

--- src/test/java/biz/netcentric/filevault/validator/aem/cloud/AemCloudValidatorTest.java
+++ src/test/java/biz/netcentric/filevault/validator/aem/cloud/AemCloudValidatorTest.java
@@ -1,5 +1,18 @@
 package biz.netcentric.filevault.validator.aem.cloud;
 
+/*-
+ * #%L
+ * AEM Cloud Validator
+ * %%
+ * Copyright (C) 2021 Netcentric - A Cognizant Digital Business
+ * %%
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * #L%
+ */
+
 import java.nio.file.Paths;
 import java.util.Collection;
 
